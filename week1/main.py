7109cd07b11bd8b2fa73184f38ad1cd277b21922
johnnynunez <johnnynuca14@gmail.com>
2023-03-10 11:37:28 +0100

    .

---
+++ week1/main.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+
+import yaml
+
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', required=True, type=str, help='yolo, rcnn or ssd')
+    parser.add_argument("-c", '--configs', default="configs/config.yml")
+    args = parser.parse_args()
+
+    # get the path of this file
+    path = os.path.dirname(os.path.realpath(__file__))
+    path_config = os.path.join(path, args.config)
+
+    with open(path_config, "r") as f:
+        config = yaml.safe_load(f)
+    main()
