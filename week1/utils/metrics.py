92016feb7f28514a0a071dcb1ec0face125faa6f
johnnynunez <johnnynuca14@gmail.com>
2023-03-12 12:54:07 +0100

    .

--- week1/utils/metrics.py
+++ week1/utils/metrics.py
@@ -1,19 +1,9 @@
 
 # Intersection over Union (IoU)
-def iou(pred, target):
-    """Calculate the Intersection over Union (IoU) of two segmentation masks.
-
-    Args:
-        pred (torch.Tensor): predicted mask, shape (N, H, W)
-        target (torch.Tensor): target mask, shape (N, H, W)
-
-    Returns:
-        torch.Tensor: IoU, shape (N,)
-    """
-    intersection = (pred & target).float().sum((1, 2))
-    union = (pred | target).float().sum((1, 2))
-    iou = (intersection + 1e-6) / (union + 1e-6)
-    return iou
+def iou():
+    pass
 
 
 # Average Precision (AP) for Object Detection
+def ap():
+    pass
