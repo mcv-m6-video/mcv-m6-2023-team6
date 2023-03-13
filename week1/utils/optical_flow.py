09eae57cebbbc5740b09889a311399b4edd091a6
guillem-cf <1659365@uab.cat>
2023-03-13 14:27:26 +0100

    optical flow

---
+++ week1/utils/optical_flow.py
@@ -0,0 +1,4 @@
+#Optical flow estimations using the Lucas-Kanade algorithm.
+# Sequences 45 and 157 (image_0) from the KITTI dataset.
+# Only 1 estimation / sequence (2 frames!)
+# Check the KITTI website for code to read results (dense motion vectors)
