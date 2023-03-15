cf35b0ad9d6e63ed4a5d0f2a35fb5779aa5354bc
johnnynunez <johnnynuca14@gmail.com>
2023-03-15 12:26:18 +0100

    .

--- week1/video2gif.py
+++ week1/video2gif.py
@@ -16,14 +16,17 @@ def video2gif(video_path, gif_path, start_time, duration, fps):
     # resize the video
     video = video.resize(0.5)
     # get the gif
-    gif = video.subclip(start_time, start_time + duration)
-    gif.write_gif(gif_path, fps=fps)
+    if duration is not None:
+        gif = video.subclip(start_time, start_time + duration)
+    else:
+        gif = video.subclip(start_time)
+    gif.speedx(10).to_gif(gif_path, fps=fps)
 
 
-video2gif('./runs/noisy/video.mp4', './runs/noisy/video.gif', 0, 20, 5)
-video2gif('./runs/rcnn/video.mp4', './runs/rcnn/video.gif', 0, 20, 5)
-video2gif('./runs/ssd/video.mp4', './runs/ssd/video.gif', 0, 20, 5)
-video2gif('./runs/yolo/video.mp4', './runs/yolo/video.gif', 0, 20, 5)
+# video2gif('./runs/noisy/video.mp4', './runs/noisy/video.gif', 0, None, 5)
+# video2gif('./runs/rcnn/video.mp4', './runs/rcnn/video.gif', 0, None, 5)
+# video2gif('./runs/ssd/video.mp4', './runs/ssd/video.gif', 0, None, 5)
+# video2gif('./runs/yolo/video.mp4', './runs/yolo/video.gif', 0, None, 5)
 
 
 # resize gif
@@ -44,10 +47,10 @@ def resize_gif(gif_path, new_gif_path, start_time, duration, fps):
         gif = gif.subclip(start_time, start_time + duration)
     else:
         gif = gif.subclip(start_time)
-    gif.write_gif(new_gif_path, fps=fps)
+    gif.speedx(10).to_gif(new_gif_path, fps=fps)
 
-resize_gif('./runs/noisy/iou.gif', './runs/noisy/iou_resize.gif', 0, 100, 5)
-resize_gif('./runs/rcnn/iou.gif', './runs/rcnn/iou_resize.gif', 0, 100, 5)
-resize_gif('./runs/ssd/iou.gif', './runs/ssd/iou_resize.gif', 0, 100, 5)
-resize_gif('./runs/yolo/iou.gif', './runs/yolo/iou_resize.gif', 0, 100, 5)
+resize_gif('./runs/noisy/iou.gif', './runs/noisy/iou_resize.gif', 0, None, 5)
+resize_gif('./runs/rcnn/iou.gif', './runs/rcnn/iou_resize.gif', 0, None, 5)
+resize_gif('./runs/ssd/iou.gif', './runs/ssd/iou_resize.gif', 0, None, 5)
+resize_gif('./runs/yolo/iou.gif', './runs/yolo/iou_resize.gif', 0, None, 5)
 
