4e971201c7fa4e895fbda82af50bacfc0b235d76
johnnynunez <johnnynuca14@gmail.com>
2023-03-20 13:48:56 +0100

    .

--- week2/main.py
+++ week2/main.py
@@ -3,7 +3,7 @@ import os
 
 import yaml
 
-from models import AdaptativeGaussian, Gaussian
+from models import AdaptativeGaussian, Gaussian, SOTA
 from utils import rendering
 
 TOTAL_FRAMES_VIDEO = 2141
@@ -24,6 +24,9 @@ def main(cfg):
     elif cfg["run_mode"] == "AdaptativeGaussian":
         model = AdaptativeGaussian(cfg, frames_modelling)
 
+    elif cfg["run_mode"] == "SOTA":
+        model = SOTA(cfg['paths']['video_path'], frames_modelling, args.method)
+
     else:
         raise ValueError("Invalid run mode")
 
@@ -43,6 +46,7 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--save", default=True, type=bool, help="Save the video or not")
     parser.add_argument("-d", "--display", default=False, type=bool, help="Show the video or not")
     parser.add_argument("-p", "--percentatge", required=True, default=False, type=float, help="Percentatge of video to use background")
+    parser.add_argument("-a", "--sota_method", default="MOG", type=str, help="SOTA method to use (MOG, MOG2, LSBP, ViBE")
     args = parser.parse_args()
 
     # get the path of this file
@@ -57,5 +61,6 @@ if __name__ == "__main__":
     config["save"] = args.save
     config["display"] = args.display
     config["percentatge"] = args.percentatge
+    config["sota_method"] = args.sota_method
 
     main(config)
---
+++ week2/models/SOTA.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+
+from week2.models.BaseModel import BaseModel
+
+
+class SOTA (BaseModel):
+    def __init__(self, video_path, num_frames, p, checkpoint=None, n_jobs=-1, method='MOG'):
+        super().__init__(video_path, num_frames, checkpoint)
+
+        if method == 'MOG':
+            self.method = cv2.bgsegm.createBackgroundSubtractorMOG(history=100, nmixtures=2, backgroundRatio=0.7)
+        elif method == 'MOG2':
+            self.method = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=36, detectShadows=True)
+        elif method == 'LSBP':
+            self.method = cv2.bgsegm.createBackgroundSubtractorLSBP()
+        elif method == 'ViBe': # Extra SOTA --> variant of the ViBe algorithm called the Count-based Neighbors Technique (CNT).
+            self.method = cv2.bgsegm.createBackgroundSubtractorCNT(minPixelStability=15, useHistory=True, isParallel=False)
+        else:
+            raise Exception('Invalid method')
+    
+    def compute_next_foreground(self):
+        ok, frame = self.cap.read()
+
+        if not ok:
+            return None
+    
+        fgmask = self.method.apply(frame)
+        _, fgmask = cv2.threshold(fgmask, 128, 255, cv2.THRESH_BINARY)
+        return fgmask, frame
+
+    def model_background(self):
+        frame = self.cap.read()
+        i = 1
+        while frame is not None and i < self.num_frames:
+
+            #fgmask = self.method.apply(frame)
+            #cv2.imshow('frame',fgmask)
+            #k = cv2.waitKey(30) & 0xff
+            #if k == 27:
+            #    break
+        
+            frame = self.cap.read()
+            i += 1
+        
+        self.cap.release()
+        cv2.destroyAllWindows()
+        
+        return i
+    
+
