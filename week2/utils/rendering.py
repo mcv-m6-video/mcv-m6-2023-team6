b87ebbcab34f5cddcaf47d54e8310fe3f9aec130
anaharrismatnez <91919056+anaharrismatnez@users.noreply.github.com>
2023-03-21 01:23:25 +0100

    .

--- week2/utils/rendering.py
+++ week2/utils/rendering.py
@@ -28,8 +28,7 @@ def rendering_video(cfg, model, frames_modelling, path_results, ai_gt_path, save
     det_rects[f'f_{counter}'] = frame_bbox
     
     counter = frames_modelling
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frames_modelling - 1)
-    for i in range(TOTAL_FRAMES_VIDEO-frames_modelling):
+    for i,frames_id in enumerate(gt_rects):
         while foreground is not None:
             if cfg['display']:
                 pass
@@ -42,9 +41,9 @@ def rendering_video(cfg, model, frames_modelling, path_results, ai_gt_path, save
                 foreground = util.noise_reduction(foreground)
                 foreground_gif.append(foreground)  # ADD IMAGE GIF
                 frame_bbox = util.findBBOX(foreground)
-                det_rects[f'f_{counter}'] = frame_bbox
+                det_rects[f'f_{frames_id}'] = frame_bbox
                 # GT bounding box
-                for box in gt_rects[counter]:
+                for box in gt_rects[frames_id]:
                     cv2.rectangle(foreground, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
                 # Detected bounding box
                 for box in frame_bbox:
