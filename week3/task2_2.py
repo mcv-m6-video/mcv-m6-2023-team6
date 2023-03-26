5d06a8a0c360193180271616fb57f7fe7d16bcf1
johnnynunez <johnnynuca14@gmail.com>
2023-03-26 21:20:00 +0200

    .

--- week3/task2_2.py
+++ week3/task2_2.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+
+import imageio
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from IPython import display as dp
@@ -9,9 +11,13 @@ import time
 import cv2
 from sort.sort import *
 from utils.util import load_from_txt, discard_overlaps
+import matplotlib
+matplotlib.use('TkAgg')
+from tqdm import tqdm
 
 
 def traking(display):
+    images = []
     current_path = os.path.dirname(os.path.abspath(__file__))
     fileDetections = os.path.join(current_path, "../../dataset/AICity_data/train/S03/c010/det/det_mask_rcnn.txt")
 
@@ -25,10 +31,11 @@ def traking(display):
     if display:
         plt.ion()  # for iterative display
         fig, ax = plt.subplots(1, 2, figsize=(20, 20))
+        fig.show()
 
     mot_tracker = Sort()  # create instance of the SORT tracker
 
-    for frame_id in frame_boxes:  # all frames in the sequence
+    for frame_id in tqdm(frame_boxes):  # all frames in the sequence
 
         dets = frame_boxes[frame_id]
         dets = discard_overlaps(dets)
@@ -38,9 +45,12 @@ def traking(display):
         if display:
             fn = current_path + f'/../../dataset/AICity_data/train/S03/c010/frames/{frame_id}.jpg'
             im = io.imread(fn)
-            ax[0].imshow(im)
             ax[0].axis('off')
             ax[0].set_title('Tracked Targets')
+            ax[0].imshow(im)
+            # fig.canvas.draw()
+
+
 
         start_time = time.time()
         dets = np.array(dets)
@@ -56,6 +66,19 @@ def traking(display):
                 ax[1].add_patch(patches.Rectangle((d[0], d[1]), d[2] - d[0], d[3] - d[1], fill=False, lw=3,
                                                   ec=colours[d[4] % 32, :]))
                 ax[1].set_adjustable('box')
+
+        # fig.canvas.draw()
+        #canvas = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+        # canvas = canvas.reshape(fig.canvas.get_width_height(physical=True)[::-1] + (3,))
+        # images.append(canvas)
+        plt.savefig(f'./output/{frame_id}.jpg')
+
+
+    # imageio.mimsave("./output" + 'iou.gif', images)
+
+        # cv2.imwrite(f'./output/{frame_id}.jpg', fig.canvas.buffer_rgba())
+
+
 
     print("Total Tracking took: %.3f for %d frames or %.1f FPS" % (total_time, total_frames, total_frames / total_time))
 
