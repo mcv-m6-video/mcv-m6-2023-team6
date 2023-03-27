f79156d8ee811ce121b7c64b9e426f776bc01eb0
guillem-cf <1659365@uab.cat>
2023-03-27 12:44:22 +0200

    task2_2

--- week3/sort/sort.py
+++ week3/sort/sort.py
@@ -95,11 +95,13 @@ class KalmanBoxTracker(object):
     """
     Initialises a tracker using initial bounding box.
     """
-    #define constant velocity model
+   
     self.kf = KalmanFilter(dim_x=7, dim_z=4) 
+
+    
+    #define constant velocity model
     self.kf.F = np.array([[1,0,0,0,1,0,0],[0,1,0,0,0,1,0],[0,0,1,0,0,0,1],[0,0,0,1,0,0,0],  [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]])
     self.kf.H = np.array([[1,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,1,0,0,0,0],[0,0,0,1,0,0,0]])
-
     self.kf.R[2:,2:] *= 10.
     self.kf.P[4:,4:] *= 1000. #give high uncertainty to the unobservable initial velocities
     self.kf.P *= 10.
--- week3/task2_2.py
+++ week3/task2_2.py
@@ -12,7 +12,6 @@ import cv2
 from sort.sort import *
 from utils.util import load_from_txt, discard_overlaps
 import matplotlib
-matplotlib.use('TkAgg')
 from tqdm import tqdm
 
 
@@ -21,29 +20,33 @@ def traking(display):
     current_path = os.path.dirname(os.path.abspath(__file__))
     fileDetections = os.path.join(current_path, "../../dataset/AICity_data/train/S03/c010/det/det_mask_rcnn.txt")
 
-    colours = np.random.rand(32, 3)  # used only for display
+    colours = np.random.rand(100, 3)  # used only for display
     frame_boxes = load_from_txt(fileDetections, threshold=0.5)  # load detections
 
     total_time = 0.0
     total_frames = 0
     out = []
 
-    if display:
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        video_out = cv2.VideoWriter("./output/" + "video.mp4", fourcc, 10, (1920, 1080))
+    
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_out = cv2.VideoWriter("./output_task2_2/" + "task2_2.mp4", fourcc, 10, (1920, 1080))
 
     mot_tracker = Sort()  # create instance of the SORT tracker
+    tracker_colors = {}
 
     for frame_id in tqdm(frame_boxes):  # all frames in the sequence
 
-        dets = frame_boxes[frame_id]
+        dets = frame_boxes[frame_id] # each box is [frame,x1, y1, x2, y2, conf]
         dets = discard_overlaps(dets)
+        # from each box we extract only the x1, y1, x2, y2
+        dets = [[d[1], d[2], d[3], d[4]] for d in dets]
 
         total_frames += 1
         fn = current_path + f'/../../dataset/AICity_data/train/S03/c010/frames/{frame_id}.jpg'
         im = io.imread(fn)
         start_time = time.time()
         dets = np.array(dets)
+
         trackers = mot_tracker.update(dets)
         cycle_time = time.time() - start_time
         total_time += cycle_time
@@ -52,7 +55,16 @@ def traking(display):
 
         for d in trackers:
             d = d.astype(np.uint32)
-            cv2.rectangle(im, (d[0], d[1]), (d[2], d[3]), (0, 255, 0), 2)
+            tracker_id = d[4]
+            if tracker_id not in tracker_colors:
+                # generate a new random color for this tracker
+                tracker_colors[tracker_id] = np.random.rand(3)
+            color = tracker_colors[tracker_id]
+            #color array to tuple
+            color = (int(color[0]*255), int(color[1]*255), int(color[2]*255))
+            
+            cv2.rectangle(im, (d[0], d[1]), (d[2], d[3]),color,2)
+            cv2.putText(im, str(tracker_id), (d[0], d[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
 
         if display:
             cv2.imshow('frame', im)
@@ -61,6 +73,7 @@ def traking(display):
 
     video_out.release()
     print("Total Tracking took: %.3f for %d frames or %.1f FPS" % (total_time, total_frames, total_frames / total_time))
+    print(out)
 
 
 if __name__ == "__main__":
