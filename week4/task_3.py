67dd01476b6a7a23038c3951220aa00f236b7cc1
guillem-cf <1659365@uab.cat>
2023-04-11 21:44:48 +0200

    .

--- week4/task_3.py
+++ week4/task_3.py
@@ -187,13 +187,12 @@ if __name__ == "__main__":
     parser.add_argument('--dataset_path', type=str, default="/ghome/group03/dataset/aic19-track1-mtmc-train",
                         help='dataset')
     
-    parser.add_argument('--sequences', type=str, default="S03", help='sequences')
+    parser.add_argument('--sequences', type=str, default="S01", help='sequences')
 
-    parser.add_argument('--results_path', type=str, default='Results/Task3/',
+    parser.add_argument('--results_path', type=str, default='Results/Task3/S01',
                         help='path to save results in a csv file')
     parser.add_argument('--visualize', type=bool, default=True)
 
     args = parser.parse_args()
-
 
     task2(args)
