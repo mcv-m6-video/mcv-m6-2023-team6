3ec848b93784cfeee82dc5a41212b352b9074ae3
guillem-cf <1659365@uab.cat>
2023-04-26 12:09:46 +0200

    .

--- week5/TrackEval.py
+++ week5/TrackEval.py
@@ -1,24 +1,61 @@
+import argparse
 import os
 
-path = "/ghome/group03/mcv-m6-2023-team6/week5/Results/end2end/byTrack"
-outputPath = "/ghome/group03/mcv-m6-2023-team6/week5/TrackEval/S01/data/trackers/mot_challenge/MOT17-train/MPNTrack/data"
 
-endToEnd = True
-MTMC = False
-seq = "S03"
-cams = {"S01":['c001', 'c002', 'c003', 'c004', 'c005'], "S03": ['c010', 'c011', 'c012', 'c013', 'c014', 'c015'], "S04": ['c016', 'c017', 'c018', 'c019', 'c020','c021', 'c022', 'c023', 'c024', 'c025', 'c026', 'c027', 'c028', 'c029', 'c030', 'c031', 'c032',  'c033', 'c034', 'c035','c036',  'c037', 'c038', 'c039','c040']}
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description="evaluate the tracking")
+    parser.add_argument("--seq", type=str)
+    parser.add_argument("--endToEnd", type=str, default = "False")
+    parser.add_argument("--csv_name", type=str, default = "mtmc.csv")
+    parser.add_argument("--input", type=str)
+    parser.add_argument("--output", type=str)
+    parser.add_argument("--evaluated", type=str, default = "False")
+    parser.add_argument("--MTMC", type=str, default = "False")
+    parser.add_argument("--evalOutput", type=str, default = "/ghome/group03/mcv-m6-2023-team6/week5/Results/TrackEvalResults")
+    args = parser.parse_args()
 
-if endToEnd:
+   
+    cams = {"S01":['c001', 'c002', 'c003', 'c004', 'c005'], "S03": ['c010', 'c011', 'c012', 'c013', 'c014', 'c015'], "S04": ['c016', 'c017', 'c018', 'c019', 'c020','c021', 'c022', 'c023', 'c024', 'c025', 'c026', 'c027', 'c028', 'c029', 'c030', 'c031', 'c032',  'c033', 'c034', 'c035','c036',  'c037', 'c038', 'c039','c040']}
+
+
+    use_end2end = bool(args.endToEnd)
+    eval = bool(args.evaluated)
+    multicam = bool(args.MTMC)
+
+    if not eval:
+        print("Evaluating the tracking")
+        if use_end2end:
+            print("Using EndToEnd")
+            for folder in os.listdir(args.input):
+                if os.path.isdir(os.path.join(args.input, folder)):
+                    #copy the file named mtmc.txt to another txt file named the cam name for the seq 
+                    os.remove(os.path.join(args.output + "data/", cams[args.seq][int(folder.split("_")[0])] + ".txt"))
+                    if multicam:
+                        os.system("cp " + os.path.join(args.input, folder, "mtmc.txt") + " " + os.path.join(args.output + "data/", cams[args.seq][int(folder.split("_")[0])] + ".txt"))
+                    else:
+                        os.system("cp " + os.path.join(args.input, folder, "mot.txt") + " " + os.path.join(args.output + "data/", cams[args.seq][int(folder.split("_")[0])] + ".txt"))
+
+        else:
+            print("Not EndToEnd")
+            for file in os.listdir(args.input):
+                if file.endswith(".txt"):
+                    #copy the file named mtmc.txt to another txt file named the cam name for the seq 
+                    # delete the file output txt file before, if it already exists
+                    if multicam:
+                        os.remove(os.path.join(args.output + "data/", cams[args.seq][int(file.split("_")[0])] + ".txt"))
+                        os.system("cp " + os.path.join(args.input, file) + " " + os.path.join(args.output + "data/", cams[args.seq][int(file.split("_")[0])] + ".txt"))
+
+    else:
+        print("Moving the csv")
+        # copy the generated csv file to the folder 
+        #get the name of the csv file in the output folder
+        files = os.listdir(args.output)
+        for file in files:
+            if file.endswith(".csv"):
+                os.system("cp " + os.path.join(args.output, file) + " " + os.path.join(args.evalOutput, args.csv_name))
+
+
+
     
-    for folder in os.listdir(path):
-        #iterate the subfolders (path + folder, only if it is a folder)
-        if os.path.isdir(os.path.join(path, folder)):
-            for subfolder in os.listdir(os.path.join(path, folder)):
-                if os.path.isdir(os.path.join(path, folder, subfolder)):
-                    if MTMC:
-                        #copy the file named mtmc.txt to another txt file named the cam name for the seq 
-                        os.system("cp " + os.path.join(path, folder, subfolder, "mtmc.txt") + " " + os.path.join(outputPath, cams[seq][int(subfolder.split("_")[0])] + ".txt"))
-
-else:
-    #copy the file named mtmc.txt to another txt file named the cam name for the seq 
-    os.system("cp " + os.path.join(path, "mtmc.txt") + " " + os.path.join(outputPath, cams[seq][0] + ".txt"))
